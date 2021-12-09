9a10090f015c77feb9f001f9e73faafcdd7e7ec6
Khushal Sahni <kstheking0@gmail.com>
2021-12-09 16:30:33 +0530

    yay

--- 2.py
+++ 2.py
@@ -1,4 +1,7 @@
 import cv2
+import numpy as np
+
+kernel = np.ones((5, 5), np.uint8)
 
 img = cv2.imread("assets/girl.png")
 
@@ -6,10 +9,28 @@ imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # cvt i.e. convert color , BGR b
 # cv2 
 imgBlur = cv2.GaussianBlur(img, (7, 7), 0) # src ksize sigmaX
 
+imgCanny = cv2.Canny(img, 150, 200) # edges
+
+imgDialation = cv2.dilate(imgCanny, kernel, iterations = 1)
+
+imgErosion = cv2.erode(imgDialation, kernel, iterations = 1)
+
 cv2.imshow("Output", imgGray)
 
 cv2.waitKey(1000)
 
 cv2.imshow("Output", imgBlur)
+
+cv2.waitKey(1000)
+
+cv2.imshow("Output", imgCanny)
+
+cv2.waitKey(1000)
+
+cv2.imshow("Output", imgDialation)
+
+cv2.waitKey(1000)
+
+cv2.imshow("Output", imgErosion)
 
 cv2.waitKey(1000)
